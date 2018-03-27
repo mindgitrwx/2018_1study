e1b0ff34e59a31efb0171564694330b7e00ea37e
jongHyeon yeo <jonghyeon.rw@gmail.com>
2018-03-27 20:49:09 +0900

    mod - separeated header file

---
+++ fpga/fpga_info.h
@@ -0,0 +1,29 @@
+#ifndef FPGA_INFO_H
+#define FPGA_INFO_H
+
+#define FPGA_BASEADDRESS        0x08000000
+#define LED_OFFSET	0x16
+#define LED_1 "./fpga_test_led 1"
+#define LED_2 "./fpga_test_led 2"
+#define LED_3 "./fpga_test_led 3"
+#define LED_4 "./fpga_test_led 4"
+#define LED_5 "./fpga_test_led 5"
+#define LED_6 "./fpga_test_led 6"
+#define LED_7 "./fpga_test_led 7"
+#define LED_8 "./fpga_test_led 8"
+#define SWITCH "./fpga_test_push_switch"
+
+#define SEVENSEG "./fpga_test_fnd"
+#define SEVENSEG_2018 "./fpga_test_fnd 2018"
+#define SEVENSEG_INIT "./fpga_test_fnd 0000"
+#define MAX_BUTTON 9
+
+#define DEV_NUM_SWITCH 3
+#define DEV_NUM_LED 4
+#define DEV_NUM_FND 5
+#define DEV_NUM_DOT 6
+#define DEV_NUM_TEXT 7
+
+#endif /** FPGA_INFO_H */
+
+  
